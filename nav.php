6724263f9460bb0622103cbbc401d9343210cd6d
yadicruz <yadicruz@users.noreply.github.com>
2021-05-23 17:43:45 -0500

    modificando localmente el archivo nav

---
+++ nav.php
@@ -0,0 +1,11 @@
+<header>
+<<ol>
+    
+
+<li><a href="#">Home</a></li>
+<li><a href="#">quienes Somos</a></li>
+<li><a href="#">Nosotros</a></li>
+<li><a href="#">Contactos</a></li>
+<li><a href="#">Ayuda</a></li>
+</ol>
+</header>
