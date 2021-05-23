b3cf55290f7294219f0e9ad1bc396cb684e25afc
yadira-cruz <84687055+yadira-cruz@users.noreply.github.com>
2021-05-23 17:39:18 -0500

    Update nav.php

---
+++ nav.php
@@ -0,0 +1,7 @@
+<header>
+<li><a href="#">Home</a></li>
+<li><a href="#">quienes Somos</a></li>
+<li><a href="#">Nosotros</a></li>
+<li><a href="#">Contactos</a></li>
+<li><a href="#">Ayuda</a></li>
+</header>
